19a7b2e3ae024a77543dac9cd12e96aac31979b3
Leaze <leaze@mailbox.org>
2023-03-11 20:34:56 +0100

    mm4 files not added to previous commit

---
+++ levels/castle_inside/model.inc.c
@@ -0,0 +1,212 @@
+Lights1 castle_inside_dl_death_plane_layer5_area1_lights = gdSPDefLights1(
+	0x7F, 0x7F, 0x7F,
+	0xFF, 0xFF, 0xFF, 0x28, 0x28, 0x28);
+
+Lights1 castle_inside_dl_bricks_grey_lights = gdSPDefLights1(
+	0x7F, 0x7F, 0x7F,
+	0xFF, 0xFF, 0xFF, 0x28, 0x28, 0x28);
+
+Gfx castle_inside_dl_black_16_16_ia8_aligner[] = {gsSPEndDisplayList()};
+u8 castle_inside_dl_black_16_16_ia8[] = {
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+	
+};
+
+Gfx castle_inside_dl_stone_bricks_grey_ci4_aligner[] = {gsSPEndDisplayList()};
+u8 castle_inside_dl_stone_bricks_grey_ci4[] = {
+	0x01, 0x10, 0x02, 0x01, 0x31, 0x14, 0x50, 0x30, 
+	0x67, 0x87, 0x35, 0x67, 0x69, 0x81, 0x27, 0x76, 
+	0x68, 0x97, 0x02, 0x66, 0x78, 0x70, 0x27, 0x66, 
+	0xaa, 0x55, 0xab, 0xc2, 0xbd, 0xda, 0xb2, 0x5a, 
+	0x10, 0x50, 0x8e, 0x01, 0x45, 0x01, 0x10, 0x01, 
+	0x71, 0x58, 0x97, 0x77, 0x15, 0x67, 0x86, 0x77, 
+	0x70, 0xb6, 0x86, 0x67, 0x0b, 0x76, 0x79, 0x87, 
+	0xda, 0xbc, 0xc2, 0xdd, 0xbc, 0xc2, 0x5a, 0xaa, 
+	0x01, 0x14, 0x23, 0x71, 0x8e, 0x01, 0x4f, 0xa0, 
+	0x66, 0x71, 0x57, 0x69, 0x96, 0x67, 0x71, 0x57, 
+	0x86, 0x70, 0xa6, 0x68, 0x76, 0x68, 0x70, 0x57, 
+	0xb2, 0xcd, 0xac, 0xc2, 0x5b, 0xba, 0xad, 0x5b, 
+	0x10, 0xd0, 0x0e, 0x11, 0x04, 0xd0, 0x01, 0x01, 
+	0x71, 0x57, 0x97, 0x76, 0x71, 0xa6, 0x79, 0x86, 
+	0x70, 0x27, 0x89, 0x67, 0x70, 0xd7, 0x98, 0x77, 
+	0x52, 0x22, 0xcc, 0x25, 0xda, 0xaa, 0xc2, 0x55, 
+	
+};
+
+Gfx castle_inside_dl_stone_bricks_grey_ci4_pal_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 castle_inside_dl_stone_bricks_grey_ci4_pal_rgba16[] = {
+	0x8c, 0x63, 0x84, 0x21, 0x4a, 0xcd, 0x94, 0xa5, 
+	0x9c, 0xe7, 0x63, 0x19, 0x73, 0x9d, 0x7b, 0xdf, 
+	0x6b, 0xd1, 0x63, 0x93, 0x6b, 0x19, 0x5a, 0xd7, 
+	0x53, 0x0f, 0x6b, 0x5b, 0x7c, 0x57, 0xa5, 0x29, 
+	
+};
+
+Vtx castle_inside_dl_Plane_mesh_layer_5_vtx_0[4] = {
+	{{ {-8500, 0, 8500}, 0, {-16, 496}, {0, 127, 0, 255} }},
+	{{ {8500, 0, 8500}, 0, {496, 496}, {0, 127, 0, 255} }},
+	{{ {8500, 0, -8500}, 0, {496, -16}, {0, 127, 0, 255} }},
+	{{ {-8500, 0, -8500}, 0, {-16, -16}, {0, 127, 0, 255} }},
+};
+
+Gfx castle_inside_dl_Plane_mesh_layer_5_tri_0[] = {
+	gsSPVertex(castle_inside_dl_Plane_mesh_layer_5_vtx_0 + 0, 4, 0),
+	gsSP1Triangle(0, 1, 2, 0),
+	gsSP1Triangle(0, 2, 3, 0),
+	gsSPEndDisplayList(),
+};
+
+Vtx castle_inside_dl_PlatformLg_mesh_layer_1_vtx_0[24] = {
+	{{ {-300, -70, 300}, 0, {1689, 240}, {129, 0, 0, 255} }},
+	{{ {-300, 70, 300}, 0, {1337, 240}, {129, 0, 0, 255} }},
+	{{ {-300, 70, -300}, 0, {1337, 1748}, {129, 0, 0, 255} }},
+	{{ {-300, -70, -300}, 0, {1689, 1748}, {129, 0, 0, 255} }},
+	{{ {-300, -70, -300}, 0, {1337, 240}, {0, 0, 129, 255} }},
+	{{ {-300, 70, -300}, 0, {986, 240}, {0, 0, 129, 255} }},
+	{{ {300, 70, -300}, 0, {986, 1748}, {0, 0, 129, 255} }},
+	{{ {300, -70, -300}, 0, {1337, 1748}, {0, 0, 129, 255} }},
+	{{ {300, -70, -300}, 0, {986, 240}, {127, 0, 0, 255} }},
+	{{ {300, 70, -300}, 0, {634, 240}, {127, 0, 0, 255} }},
+	{{ {300, 70, 300}, 0, {634, 1748}, {127, 0, 0, 255} }},
+	{{ {300, -70, 300}, 0, {986, 1748}, {127, 0, 0, 255} }},
+	{{ {300, -70, 300}, 0, {634, 240}, {0, 0, 127, 255} }},
+	{{ {300, 70, 300}, 0, {282, 240}, {0, 0, 127, 255} }},
+	{{ {-300, 70, 300}, 0, {282, 1748}, {0, 0, 127, 255} }},
+	{{ {-300, -70, 300}, 0, {634, 1748}, {0, 0, 127, 255} }},
+	{{ {-300, -70, -300}, 0, {-1226, 1748}, {0, 129, 0, 255} }},
+	{{ {300, -70, -300}, 0, {282, 1748}, {0, 129, 0, 255} }},
+	{{ {300, -70, 300}, 0, {282, 240}, {0, 129, 0, 255} }},
+	{{ {-300, -70, 300}, 0, {-1226, 240}, {0, 129, 0, 255} }},
+	{{ {300, 70, -300}, 0, {-1226, 240}, {0, 127, 0, 255} }},
+	{{ {-300, 70, -300}, 0, {282, 240}, {0, 127, 0, 255} }},
+	{{ {-300, 70, 300}, 0, {282, -1268}, {0, 127, 0, 255} }},
+	{{ {300, 70, 300}, 0, {-1226, -1268}, {0, 127, 0, 255} }},
+};
+
+Gfx castle_inside_dl_PlatformLg_mesh_layer_1_tri_0[] = {
+	gsSPVertex(castle_inside_dl_PlatformLg_mesh_layer_1_vtx_0 + 0, 16, 0),
+	gsSP1Triangle(0, 1, 2, 0),
+	gsSP1Triangle(0, 2, 3, 0),
+	gsSP1Triangle(4, 5, 6, 0),
+	gsSP1Triangle(4, 6, 7, 0),
+	gsSP1Triangle(8, 9, 10, 0),
+	gsSP1Triangle(8, 10, 11, 0),
+	gsSP1Triangle(12, 13, 14, 0),
+	gsSP1Triangle(12, 14, 15, 0),
+	gsSPVertex(castle_inside_dl_PlatformLg_mesh_layer_1_vtx_0 + 16, 8, 0),
+	gsSP1Triangle(0, 1, 2, 0),
+	gsSP1Triangle(0, 2, 3, 0),
+	gsSP1Triangle(4, 5, 6, 0),
+	gsSP1Triangle(4, 6, 7, 0),
+	gsSPEndDisplayList(),
+};
+
+Gfx mat_castle_inside_dl_death_plane_layer5_area1[] = {
+	gsDPPipeSync(),
+	gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, TEXEL0, 0, PRIMITIVE, 0, 0, 0, 0, COMBINED, 0, 0, 0, COMBINED),
+	gsDPSetFogColor(0, 0, 0, 255),
+	gsSPFogPosition(985, 1000),
+	gsSPSetGeometryMode(G_FOG),
+	gsSPClearGeometryMode(G_CULL_BACK),
+	gsDPSetCycleType(G_CYC_2CYCLE),
+	gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_XLU_SURF2),
+	gsSPTexture(65535, 65535, 0, 0, 1),
+	gsDPSetPrimColor(0, 0, 255, 255, 255, 255),
+	gsSPSetLights1(castle_inside_dl_death_plane_layer5_area1_lights),
+	gsDPSetTextureImage(G_IM_FMT_IA, G_IM_SIZ_8b_LOAD_BLOCK, 1, castle_inside_dl_black_16_16_ia8),
+	gsDPSetTile(G_IM_FMT_IA, G_IM_SIZ_8b_LOAD_BLOCK, 0, 0, 7, 0, G_TX_WRAP | G_TX_NOMIRROR, 4, 0, G_TX_WRAP | G_TX_NOMIRROR, 4, 0),
+	gsDPLoadBlock(7, 0, 0, 127, 1024),
+	gsDPSetTile(G_IM_FMT_IA, G_IM_SIZ_8b, 2, 0, 0, 0, G_TX_WRAP | G_TX_NOMIRROR, 4, 0, G_TX_WRAP | G_TX_NOMIRROR, 4, 0),
+	gsDPSetTileSize(0, 0, 0, 60, 60),
+	gsSPEndDisplayList(),
+};
+
+Gfx mat_revert_castle_inside_dl_death_plane_layer5_area1[] = {
+	gsDPPipeSync(),
+	gsSPClearGeometryMode(G_FOG),
+	gsSPSetGeometryMode(G_CULL_BACK),
+	gsDPSetCycleType(G_CYC_1CYCLE),
+	gsDPSetRenderMode(G_RM_AA_ZB_XLU_SURF, G_RM_AA_ZB_XLU_SURF2),
+	gsSPEndDisplayList(),
+};
+
+Gfx mat_castle_inside_dl_bricks_grey[] = {
+	gsDPPipeSync(),
+	gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT, TEXEL0, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT),
+	gsSPTexture(65535, 65535, 0, 0, 1),
+	gsSPSetLights1(castle_inside_dl_bricks_grey_lights),
+	gsDPSetTextureLUT(G_TT_RGBA16),
+	gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, castle_inside_dl_stone_bricks_grey_ci4_pal_rgba16),
+	gsDPSetTile(0, 0, 0, 256, 7, 0, G_TX_WRAP | G_TX_NOMIRROR, 0, 0, G_TX_WRAP | G_TX_NOMIRROR, 0, 0),
+	gsDPLoadTLUTCmd(7, 15),
+	gsDPLoadSync(),
+	gsDPSetTextureImage(G_IM_FMT_CI, G_IM_SIZ_16b, 1, castle_inside_dl_stone_bricks_grey_ci4),
+	gsDPSetTile(G_IM_FMT_CI, G_IM_SIZ_16b, 0, 0, 7, 0, G_TX_WRAP | G_TX_NOMIRROR, 4, 0, G_TX_WRAP | G_TX_NOMIRROR, 4, 0),
+	gsDPLoadBlock(7, 0, 0, 63, 2048),
+	gsDPSetTile(G_IM_FMT_CI, G_IM_SIZ_4b, 1, 0, 0, 0, G_TX_WRAP | G_TX_NOMIRROR, 4, 0, G_TX_WRAP | G_TX_NOMIRROR, 4, 0),
+	gsDPSetTileSize(0, 0, 0, 60, 60),
+	gsSPEndDisplayList(),
+};
+
+Gfx mat_revert_castle_inside_dl_bricks_grey[] = {
+	gsDPPipeSync(),
+	gsDPSetTextureLUT(G_TT_NONE),
+	gsSPEndDisplayList(),
+};
+
+Gfx castle_inside_dl_Plane_mesh_layer_5[] = {
+	gsSPDisplayList(mat_castle_inside_dl_death_plane_layer5_area1),
+	gsSPDisplayList(castle_inside_dl_Plane_mesh_layer_5_tri_0),
+	gsSPDisplayList(mat_revert_castle_inside_dl_death_plane_layer5_area1),
+	gsSPEndDisplayList(),
+};
+
+Gfx castle_inside_dl_PlatformLg_mesh_layer_1[] = {
+	gsSPDisplayList(mat_castle_inside_dl_bricks_grey),
+	gsSPDisplayList(castle_inside_dl_PlatformLg_mesh_layer_1_tri_0),
+	gsSPDisplayList(mat_revert_castle_inside_dl_bricks_grey),
+	gsSPEndDisplayList(),
+};
+
+Gfx castle_inside_dl_material_revert_render_settings[] = {
+	gsDPPipeSync(),
+	gsSPSetGeometryMode(G_LIGHTING),
+	gsSPClearGeometryMode(G_TEXTURE_GEN),
+	gsDPSetCombineLERP(0, 0, 0, SHADE, 0, 0, 0, ENVIRONMENT, 0, 0, 0, SHADE, 0, 0, 0, ENVIRONMENT),
+	gsSPTexture(65535, 65535, 0, 0, 0),
+	gsDPSetEnvColor(255, 255, 255, 255),
+	gsDPSetAlphaCompare(G_AC_NONE),
+	gsSPEndDisplayList(),
+};
+
